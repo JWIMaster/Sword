aa2826add3d468cf360927f31b06e02605ad2bb0
JWI <jwi@j-w-i.org>
2025-10-14 20:38:36 +1100

    a

--- Sources/Sword/Rest/Request.swift
+++ Sources/Sword/Rest/Request.swift
@@ -46,7 +46,7 @@ extension Sword {
       route += ".delete"
     }
 
-    var urlString = "https://discord.com/api/v6\(endpointInfo.url)"
+    var urlString = "https://discordapp.com/api/v9\(endpointInfo.url)"
 
     if let params = params {
       urlString += "?"
